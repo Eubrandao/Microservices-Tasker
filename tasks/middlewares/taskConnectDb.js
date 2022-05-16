39a56ddd796894d5edf327aa456071a5cf39eaea
Wellington Willians Barbieri Brandão <wellington.brandao@simbiox.com.br>
2022-05-15 21:54:25 -0300

    Add Kafka

--- tasks/middlewares/taskConnectDb.js
+++ tasks/middlewares/taskConnectDb.js
@@ -2,11 +2,10 @@ import mongoose from "mongoose";
 
 const db = mongoose.connection;
 
-const { DB_CONNECTION_STRING } = process.env;
+import dotenv from "dotenv";
+dotenv.config();
 
-mongoose.connect(
-  "mongodb+srv://well:Qwer456@cluster0.qvutp.mongodb.net/task-manager?retryWrites=true&w=majority"
-);
+mongoose.connect(process.env.DB_CONNECTION_STRING);
 mongoose.connection.on("connected", () => console.log("Connected to DB"));
 mongoose.connection.on("error", (error) =>
   console.log("Failed to connected in DB" + error)
